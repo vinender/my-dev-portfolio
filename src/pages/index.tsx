b1148ca07fff1c818e3b04f1d532bb07149e3b53
vinender <vinendersingh91@gmail.com>
2024-10-05 00:44:36 +0530

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,7 @@ import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
-import AIAcknowledgment from '@/components/Acknowledgement';
-
+ 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
